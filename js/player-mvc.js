9d7e1440857db1941980cc2281e9881501886e88
falconxtreme <falcon.donovan@gmail.com>
2015-04-18 13:34:18 -0500

    f

--- js/player-mvc.js
+++ js/player-mvc.js
@@ -22,7 +22,7 @@ Model.prototype.setActive = function(index){
 	if(index<0 || index>= this.songs.length) index=0
 	this._isPlaying = true
 	this.actual=index
-	this.trigger('active-songs', this.songs[index])
+	this.trigger('active-song', this.songs[index])
 }
 Model.prototype.setPlaying = function(bool){
 	this._isPlaying=bool
@@ -65,9 +65,96 @@ function View(){
 	})
 
 	//creación de mi template de canciones
-	this.template = Handlebars.compile($('#song-tmpl')).html())
+	this.template = Handlebars.compile($('#song-tmpl').html())
 }
 View.prototype= new Observable()
 View.prototype.updateList= function(songList){
 	this.elems.list.html(this.template({songs: songList}))
 }
+View.prototype.updateActive = function(song){
+	this.elems.avatar.attr('src', song.img)
+	this.elems.name.text(song.name)
+	this.elems.artist.text(song.artist)
+
+	this.elems.audio.pause()
+	this.elems.audio.src=song.src
+	this.elems.audio.play()
+}
+View.prototype.playSong=function(){
+	this.elems.audio.play()
+}
+View.prototype.pauseSong=function(){
+	this.elems.audio.pause()
+}
+
+var controller={
+	init: function(){
+		var self=this
+
+		//instancio mi modelo y vista
+		this.model = new Model()
+		this.view = new View()
+
+		//Escuchar eventos del modelo
+		this.model.on('song-changed', function(e,songs){
+			self.view.updateList(songs);
+		})
+		this.model.on('active-song', function(e,song){
+			self.view.updateActive(song)
+		})
+		this.model.on('playing', function(e, song){
+			self.view.playSong(song)
+		})
+		this.model.on('paused', function(e, song){
+			self.view.pauseSong(song)
+		})
+
+		//Escuchar eventos de la vista
+		this.view.on('song-clicked', function(e, index){
+			self.model.setActive(index)
+		})
+		this.view.on('play-clicked', function(){
+			self.model.setPlaying(!self.model.isPlaying())
+		})
+		this.view.on('next-clicked', function(){
+			self.model.goTo(self.model.getActualIndex()+1)
+		})
+		this.view.on('prev-clicked', function(){
+			self.model.goTo(self.model.getActualIndex()-1)
+		})
+
+		//Inicializacion
+		this.model.setSongs([
+			{
+				name: 'Ill Be My Mirror',
+				artist: '8in8',
+				src:'media/8in8_Ill_Be_My_Mirror.mp3',
+				img: 'img/songs/girl_pharrel_williams.jpg' 
+			},
+			{
+				name: 'Floss Suffers From Ganma_Radiation',
+				artist: 'Blue_Ducks',
+				src:'media/Floss_Suffers.mp3',
+				img: 'img/songs/girl_pharrel_williams.jpg' 
+			},
+			{
+				name: 'IMF',
+				artist: 'DOT',
+				src:'media/IMF.mp3',
+				img: 'img/songs/girl_pharrel_williams.jpg' 
+			}
+		])
+		this.model.setActive(0)
+	},
+	next: function(){
+
+	},
+	prev: function(){
+
+	},
+	pause: function(){
+
+	}
+}
+
+controller.init()
